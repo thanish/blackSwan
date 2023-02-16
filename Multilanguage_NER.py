392c8b585a1f673d0e53ba69c84d685f8a4945d0
Thanish <thanish1991@yahoo.co.in>
2023-02-16 17:22:49 +0530

    Update Multilanguage_NER.py

--- Multilanguage_NER.py
+++ Multilanguage_NER.py
@@ -3,11 +3,11 @@ import spacy
 
 def get_entities(text, language_code):
         
-    if language_code in ['ca', 'hr', 'da', 'nl', 'fi', 'fr', 'de',
+    if language_code.lower() in ['ca', 'hr', 'da', 'nl', 'fi', 'fr', 'de',
                         'el','it', 'ja', 'ko', 'lt', 'mk','nb',
                         'pl','pt','ro','ru','es','sv','uk']:
         model = f"{language_code}_core_news_sm"
-    elif language_code in ['zh', 'en']:
+    elif language_code.lower() in ['zh', 'en']:
         model = f"{language_code}_core_web_sm"
     else:
         model = f"xx_ent_wiki_sm"
